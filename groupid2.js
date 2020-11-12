e6637a42f4eff1764809a8a68c74d942b188c96d
COD <ranggaanggrawan@gmail.com>
2020-11-12 14:35:14 +0700

    Update groupid2.js

--- groupid2.js
+++ groupid2.js
@@ -1,7 +1,6 @@
 /* Daftar Pengguna Template */
 var twLSN = [
-'waorder11.blogspot.com',
-'chartpediaa.blogspot.com',
+'chartpedia.blogspot.com',
 ];
 /* URL Pemilik Lisensi */
 var redirectURL = 'http://ardesign.com';
