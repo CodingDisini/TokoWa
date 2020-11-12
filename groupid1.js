3712cdbb62885486b9612c74a866d5c40002220d
COD <ranggaanggrawan@gmail.com>
2020-11-12 14:44:44 +0700

    Update groupid1.js

--- groupid1.js
+++ groupid1.js
@@ -1,4 +1,5 @@
 /*
 "Ingatlah, laknat Allah (ditimpakan) atas orang-orang yang zalim" (QS. Hud: 18).
 */
-let arrWEB = ['192.168.20.2020',];
+let arrWEB = ['chartpedia.blogspot.com',
+             ];
