a0405ddb18aa7c2db852e4ae829bd6a005a92ee2
COD <ranggaanggrawan@gmail.com>
2021-02-24 00:46:05 +0700

    Update groupid2.js

--- groupid2.js
+++ groupid2.js
@@ -1,1 +1,1 @@
-let arrWEB = ['192.168.20.220','siplah.barungoding.com','lapak.ardesign.web.id','www.m.ardesign.web.id','lapak.ariefcenter.id',];
+let arrWEB = ['192.168.20.220','siplah.barungoding.com','lapak.ardesign.web.id','www.m.ardesign.web.id','lapak.ariefcenter.id','lapak.cintasuci.my.id',];
